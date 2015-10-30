53b6c7db944e34f867035bbd2fa465f7845f9cbc
guipimenta <pimenta14@gmail.com>
2015-10-30 11:57:17 -0200

    File as args

--- lexico/main.c
+++ lexico/main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
   //printf("Reading file: %s \n", argv[1]);
 
-  read_file("ENTRADA.txt");
+  read_file(argv[1]);
 
   // print_reserved_words_table();
   // print_single_symbols_table();
